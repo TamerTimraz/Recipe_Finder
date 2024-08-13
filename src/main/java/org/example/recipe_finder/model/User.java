318f08636cd75472087319a62d337148f0a751e9
TamerTimraz <tamertimraz04@gmail.com>
2024-08-13 14:09:07 -0400

    Created the User entity class

---
+++ src/main/java/org/example/recipe_finder/model/User.java
@@ -0,0 +1,20 @@
+package org.example.recipe_finder.model;
+
+import jakarta.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<SavedRecipe> savedRecipes;
+
+}
