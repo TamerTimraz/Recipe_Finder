324b5ff0bce0daacc3e609b0053fd7028009d81f
TamerTimraz <tamertimraz04@gmail.com>
2024-08-14 17:42:35 -0400

    Added getters and setters

--- src/main/java/org/example/recipe_finder/model/User.java
+++ src/main/java/org/example/recipe_finder/model/User.java
@@ -11,10 +11,36 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<SavedRecipe> savedRecipes;
 
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
