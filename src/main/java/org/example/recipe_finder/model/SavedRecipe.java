cd61a3fae18a23e5165cca936ef8e65afba064a1
TamerTimraz <tamertimraz04@gmail.com>
2024-08-16 17:53:43 -0400

    Added getters and setters

--- src/main/java/org/example/recipe_finder/model/SavedRecipe.java
+++ src/main/java/org/example/recipe_finder/model/SavedRecipe.java
@@ -17,4 +17,36 @@ public class SavedRecipe {
     private Long recipeId;
 
     private String recipeTitle;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(Long recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public String getRecipeTitle() {
+        return recipeTitle;
+    }
+
+    public void setRecipeTitle(String recipeTitle) {
+        this.recipeTitle = recipeTitle;
+    }
 }
