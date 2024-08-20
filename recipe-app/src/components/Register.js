6bce8d55a8a36c7ad5934dadc212d0e46db91411
TamerTimraz <tamertimraz04@gmail.com>
2024-08-20 15:19:40 -0400

    Created register form for registering a user

--- recipe-app/src/components/Register.js
+++ recipe-app/src/components/Register.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Register(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if(username.length < 3){
+            setMessage("Username must be at least 3 characters long.");
+            return;
+        }
+
+        if(password.length < 8){
+            setMessage("Password must be at least 8 characters long.")
+        }
+
+        try {
+            const response = await axios.post('http://localhost:8080/api/users/register', {
+                username: username,
+                password: password
+            });
+            setMessage('User registered successfully!');
+        } catch(error){
+            setMessage('Failed to register user.');
+        }
+    };
+
     return(
         <div>
             <h2>Register Page</h2>
-            {/*Registration form goes here*/}
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    />
+                <input
+                    type="text"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    />
+                <button type="submit">Register</button>
+            </form>
+            {message && <p>{message}</p>}
         </div>
     );
 }
