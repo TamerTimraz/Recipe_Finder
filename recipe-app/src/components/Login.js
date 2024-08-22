f10a143b8500a431c069093f20dd7c68ea5be943
TamerTimraz <tamertimraz04@gmail.com>
2024-08-21 20:09:24 -0400

    Created Login form

--- recipe-app/src/components/Login.js
+++ recipe-app/src/components/Login.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 function Login(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post("http://localhost:8080/api/users/login", {
+                username: username,
+                password: password
+            });
+            if(response.data.equals("Login successful")){
+                // Redirect to main application
+            } else {
+                setMessage(response.data);
+            }
+        } catch(error){
+            setMessage("Failed to login. Please try again later.");
+        }
+    }
+
     return (
         <div>
             <h2>Login Page</h2>
-            {/*Login form goes here*/}
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    />
+                <input
+                    type="text"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    />
+                <button type="submit">Login</button>
+            </form>
+            {message && <p>{message}</p>}
         </div>
     );
 }
