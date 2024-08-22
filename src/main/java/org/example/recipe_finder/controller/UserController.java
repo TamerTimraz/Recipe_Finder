3ce20aa95c6d9072d74dc55eb8a154662aaf22c5
TamerTimraz <tamertimraz04@gmail.com>
2024-08-21 20:09:01 -0400

    Added API endpoint to check credentials for logging in

--- src/main/java/org/example/recipe_finder/controller/UserController.java
+++ src/main/java/org/example/recipe_finder/controller/UserController.java
@@ -3,6 +3,8 @@ package org.example.recipe_finder.controller;
 import org.example.recipe_finder.model.User;
 import org.example.recipe_finder.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,5 +26,15 @@ public class UserController {
     @GetMapping("/{username}")
     public User getUserByUsername(@PathVariable String username) {
         return userService.findUserByUsername(username);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody User user) {
+        User existingUser = userService.findUserByUsername(user.getUsername());
+        if (existingUser != null && existingUser.getPassword().equals(user.getPassword())) {
+            return ResponseEntity.ok("Login successful");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
     }
 }
