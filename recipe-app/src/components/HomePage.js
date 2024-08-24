9e44b7ca4383b0932003ae35dd8cecbcea09f3b5
TamerTimraz <tamertimraz04@gmail.com>
2024-08-24 17:25:39 -0400

    Added RecipeSearch component to the Homepage

--- recipe-app/src/components/HomePage.js
+++ recipe-app/src/components/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import RecipeSearch from "./RecipeSearch";
 
 const HomePage = () => {
     return (
         <div>
             <h1>Welcome to the Recipe App</h1>
             <p>Here you can search for, view, and save your favorite recipes!</p>
+            <RecipeSearch />
         </div>
     );
 };
