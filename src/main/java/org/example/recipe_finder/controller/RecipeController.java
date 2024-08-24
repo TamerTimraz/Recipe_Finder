279ed3f7f845d95e051d0351ef438f543e57115a
TamerTimraz <tamertimraz04@gmail.com>
2024-08-24 15:33:28 -0400

    Created controller for api mapping between the frontend and backend

---
+++ src/main/java/org/example/recipe_finder/controller/RecipeController.java
@@ -0,0 +1,20 @@
+package org.example.recipe_finder.controller;
+
+import org.example.recipe_finder.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/recipes")
+@CrossOrigin(origins = "http://localhost3000")
+public class RecipeController {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    @GetMapping("/search")
+    public String searchRecipes(@RequestParam String query) {
+        return recipeService.searchRecipes(query);
+    }
+
+}
