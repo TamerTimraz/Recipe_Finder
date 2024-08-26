7f150ef8572f2a57a999f9015513bc30f5d774ba
TamerTimraz <tamertimraz04@gmail.com>
2024-08-26 15:55:22 -0400

    Added logout button

--- recipe-app/src/components/HomePage.js
+++ recipe-app/src/components/HomePage.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import RecipeSearch from "./RecipeSearch";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+
+    const navigate = useNavigate();
+
+    const handleLogOut = (e) => {
+        e.preventDefault();
+
+        navigate("/login")
+    }
+
+
     return (
         <div style={{textAlign: "center"}}>
+            <header className="header">
+                <button className="logout-btn" onClick="handleLogOut">Log Out</button>
+            </header>
             <h1>Welcome to the Recipe App</h1>
             <p>Here you can search for, view, and save your favorite recipes!</p>
-            <RecipeSearch />
+            <RecipeSearch/>
         </div>
+
     );
 };
 
