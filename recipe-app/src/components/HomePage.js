d2dea49a01af7bbd5a478de5a1df85c6341bb0a1
TamerTimraz <tamertimraz04@gmail.com>
2024-09-07 16:42:16 -0400

    Removed RecipeDetails component from the home page, navigating to a new page now

--- recipe-app/src/components/HomePage.js
+++ recipe-app/src/components/HomePage.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import RecipeSearch from "./RecipeSearch";
 import RecipeList from "./RecipeList";
-import RecipeDetails from "./RecipeDetails";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
     const [recipes, setRecipes] = useState([]);
-    const [selectedRecipe, setSelectedRecipe] = useState(null);
     const navigate = useNavigate();
 
     const handleLogOut = (e) => {
@@ -15,13 +13,8 @@ const HomePage = () => {
     };
 
     const handleRecipeClick = (recipe) => {
-        setSelectedRecipe(recipe);
+        navigate(`/recipes/${recipe.id}`);
     };
-
-    const handleCloseDetails = () => {
-        setSelectedRecipe(null);
-    };
-
 
     return (
         <div style={{textAlign: "center"}}>
@@ -31,11 +24,7 @@ const HomePage = () => {
             <h1>Welcome to the Recipe App</h1>
             <p>Here you can search for, view, and save your favorite recipes!</p>
             <RecipeSearch setRecipes={setRecipes} />
-            {selectedRecipe ? (
-                <RecipeDetails recipe={selectedRecipe} onClose={handleCloseDetails} />
-            ) : (
-                <RecipeList recipes={recipes} onRecipeClick={handleRecipeClick} />
-            )}
+            <RecipeList recipes={recipes} onRecipeClick={handleRecipeClick} />
         </div>
 
     );
